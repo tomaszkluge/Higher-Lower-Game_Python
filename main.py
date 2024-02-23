c0a89fae435bd2bc7732b79717f9b2fae2f09156
tomaszkluge <tomasz.kluge@gmail.com>
2024-02-23 08:35:44 +0100

    Finally version, made new def - welcome msg

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import random
 from sources.data import database
 import sources.art
 
-print(sources.art.logo)
-print("""Welcome to Higher - Lower Game!\n
-    Rules:
-    1. Computer gives you a question - which in two example has more followers
-    2. You choose between first - 1 and second - 2\n""")
+
+def welcome_in_game():
+    print(sources.art.logo)
+    print("""Welcome to Higher - Lower Game!\n
+        Rules:
+        1. Computer gives you a question - which in two example has more followers
+        2. You choose between first - 1 and second - 2\n""")
 
 
 def random_account():
     account = random.choice(database)
     account_list = [account["name"], account["description"], account["country"], account["follower_count"]]
     account_output = f"{account_list[0]} - {account_list[1]} from {account_list[2]}"
-    account_followers = account_list[3]
-    return account_output, account_followers
+    return account_output
 
 
 def game():
+    welcome_in_game()
     should_continue = True
     first_account = random_account()
     while should_continue:
